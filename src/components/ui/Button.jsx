9e2cbab934d76eeefa7d5ce40a72b63e8418b6d5
hsiaoju <hsiaojuko@liontravel.com>
2025-03-24 22:48:50 +0800

    feat: add hover animation

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ text, textSize = 'text-base' }) {
 	return (
-		<button className="gradient-border">
+		<button className="gradient-border rotate-gradient">
 			<div className={`gradient-inner ${textSize}`}>
 				{text}
 			</div>
